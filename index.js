72c8530a500a048abede04ed09b04e01fc9d4fde
Yaniv Kessler <yanivk@gmail.com>
2013-06-16 05:46:04 +0300

    exposed bare functions

---
+++ index.js
@@ -0,0 +1,4 @@
+module.exports.executeQuery = require('./lib/executeQuery');
+module.exports.loadData = require('./lib/loadData');
+module.exports.unloadData = require('./lib/unloadData');
+module.exports.searchUnloads = require('./lib/searchUnloads');
